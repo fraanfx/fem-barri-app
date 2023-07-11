265c92d63a5b2b298dd0e594ed11e39850fefcaa
fraanfx <fran.am95@gmail.com>
2023-07-11 20:01:57 +0200

    try fix dependecies bugs

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,21 +7,28 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import Header from "@/components/Header";
 
 import Footer from "@/components/Footer";
+import { Montserrat } from '@next/font/google'
+
+
+
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 // Tell Font Awesome to skip adding the CSS automatically 
 // since it's already imported above
 config.autoAddCss = false; 
 
+const montserratFont = Montserrat({
+  subsets:['latin']
+})
 export const metadata = {
   title: "FemBarri",
   description: "Descubre nuevos sitios en bcn",
 };
 
 const RootLayout = ({ children }) => (
-  <html lang='en'>
+  <html lang='es'>
     <body>
-        <main className='app'>
+        <main className={`app ${montserratFont.className}`}>
             <Header />
           {children}
         </main>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import { Meow_Script } from '@next/font/google'
+
+const meowScriptFont = Meow_Script({
+  subsets:['latin'],
+  weight: ['400']
+})
 
 const Header = () => {
   return (
     <Link href={'/'}>
     <div className='header-container'>
-         <h1 className='head_text text-center'>
+         <h1 className={`head_text text-center `}>
      <b>FEM</b>BARRI
       <br className='max-md:hidden' />
-      <span className='subtitle'> Sant Andreu</span>
+      <span className={`subtitle ${meowScriptFont.className}`}> Sant Andreu</span>
     </h1>
     </div>
     </Link>
