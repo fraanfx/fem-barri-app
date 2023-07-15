cda5420cd56c3bdceb3eca3cef8553c7b1a7680f
fraanfx <fran.am95@gmail.com>
2023-07-16 01:56:33 +0200

    Update map

--- components/iconSocial.jsx
+++ components/iconSocial.jsx
@@ -15,29 +15,28 @@ import {
 
 const IconSocial = ({type, link}) => {
   const [iconType, setIcon] = useState(faInstagram)
-    // const how = (type) => {
-    //     if(type == "facebookUrl"){
-    //         setIcon(faFacebookF)
-    //     } else if(type == "instagramUrl"){
-    //       setIcon(faInstagram)
-    //      }else if (type == "twitterUrl"){
-    //          setIcon(faTwitter);
-    //      } else if (type == "youtubeUrl"){
-    //       setIcon(faYouTube);
-    //     } else if (type == "pinterestUrl"){
-    //         setIcon(faPinterest)
-    //     }else if (type == "linkedinUrl"){
-    //         setIcon(faLinkedIn);
-    //     }else if (type == "tiktokUrl"){
-    //         setIcon(faTikTok);
-    //     } 
-    // }
-    // how()
+    const how = (type) => {
+        // if(type == "facebookUrl"){
+        //     setIcon(faFacebookF)
+        // } else if(type == "instagramUrl"){
+        //   setIcon(faInstagram)
+        //  }else if (type == "twitterUrl"){
+        //      setIcon(faTwitter);
+        //  } else if (type == "youtubeUrl"){
+        //   setIcon(faYouTube);
+        // } else if (type == "pinterestUrl"){
+        //     setIcon(faPinterest)
+        // }else if (type == "linkedinUrl"){
+        //     setIcon(faLinkedIn);
+        // }else if (type == "tiktokUrl"){
+        //     setIcon(faTikTok);
+        // } 
+        return null
+    }
+    
   return (
     <div className="icon-container">
             <a target="_blank" href={link} rel="noopener noreferrer">
-            {/* <FontAwesomeIcon icon={iconType} style={{marginRight : "6px", fontSize : "20px"}} /> */}
-   
          <p>{type} {link}</p>
         </a>
     </div>
