d5356ef798e96db6aad21aa67d646bd9f9d60494
fraanfx <fran.am95@gmail.com>
2023-07-16 01:49:26 +0200

    Update map

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,21 +1,19 @@
-import IconSocial from "@/components/IconSocial";
+// import IconSocial from "@/components/IconSocial";
 const Social = ({links}) => {
     
    
     console.log("[socialInner]",links)
   return (
    <div className="social-container">
-                    {/* {links.forEach((link, index) => (
-                       <IconSocial key={index} type={link.Object.keys()} link={link}/>
-                   ))}     */}
-                   {
+        
+                   {/* {
                     Object.entries(links).map(([key, value], idx) =>
                      (
                       <IconSocial key={idx} type={key} link={value}/>
                     
                      )
                     )
-                   }  
+                   }   */}
                   
                   
                    
